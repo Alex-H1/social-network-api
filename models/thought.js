ecf749f126ddf4961835a10d2c0c3c3a384a2644
alex hernandez <alexanderohernandez21@gmail.com>
2022-05-22 11:42:01 -0700

    added thought model

---
+++ models/thought.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./reaction');
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText:{
+            type: String,
+            Required: true,
+            maxlength: 280,
+        },
+        createdAt:{
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm:a')
+        },
+        username:{
+            type: String,
+            Required: true,
+        },
+        reactions:[reactionSchema]
+    },
+    {
+        toJSON:{
+            getters: true,
+            virtuals: true,
+        },
+        id: false
+    }
+);
+
+const Thought = model('Thought', ThoughtSchema)
+
+ThoughtSchema.virtual('reactionCount').get(function(){
+    return this.reaction.length;
+});
+
+module.export = Thought;
